89b47c914a8fadf5d9ea6eb6b37d6479edf58129
JoseAndresM <76529734+JoseAndresM@users.noreply.github.com>
2024-10-07 12:03:01 +0200

    Update lumina-auto-testing.py

--- lumina-auto-testing.py
+++ lumina-auto-testing.py
@@ -185,11 +185,11 @@ def autotesting_aggregate(new_data, target_roas_d0, target_cpi, data_source):
 
     # Define weights for each component
     weights = {
-        'scaled_cost': 0.7,
+        'scaled_cost': 1.0,
         'scaled_ROAS_diff': 1.5,
         'scaled_ROAS_Mat_D3': 1.0,
         'scaled_IPM': 1.0,
-        'scaled_CPI_diff': 1.2
+        'scaled_CPI_diff': 1.3
     }
 
     # Adjust weights if only Unity data is selected (since ROAS and CPI metrics are not available)
